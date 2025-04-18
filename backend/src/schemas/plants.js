a19fbbaf43f37577d557b255d696db29f7cc1097
Eduard Sanchez <eduardosaanchezlopez@gmail.com>
2025-04-18 19:11:49 +0200

    schemaUpdate

--- backend/src/schemas/plants.js
+++ backend/src/schemas/plants.js
@@ -5,11 +5,13 @@ export const plantSchema = z.object({
   image: z.string().optional(),
   last_watering_date: z.string().optional(),
   last_fertilize_date: z.string().optional(),
-  watering: z.string().optional().default('unknown'),
+  summer_watering: z.string().optional().default('unknown'),
+  winter_watering: z.string().optional().default('unknown'),
   sunlight: z.string().optional().default('unknown'),
   location: z.string().optional().default('unknown'),
   edible: z.string().optional().default('unknown'),
-  toxicity: z.string().optional().default('unknown'),
+  human_toxicity: z.string().optional().default('unknown'),
+  animal_toxicity: z.string().optional().default('unknown'),
   description: z.string().optional().default('No description available')
 })
 
